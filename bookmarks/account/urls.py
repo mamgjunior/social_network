1a8e8f1a5bc62cd6169717e4ae1aeeb4dd6bc325
mamgjunior <mamgjunior@hotmail.com>
2021-11-05 22:21:45 -0300

    alterando a url.py para receber as urls para a alteração de senha e também criando os templates das mesmas

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -7,6 +7,16 @@ urlpatterns = [
     # View de login
     # path('login/', views.user_login, name='login'),
     path('', views.dashboard, name='dashboard'),
+
+    # url para fazer o login na aplicação
     path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),    
+
+    # url para fazer o logout da aplicação
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+    # url para alteração de senha
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+
+    # url que confirma a alteração de senha
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
 ]
