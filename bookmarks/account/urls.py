26cf9b12c24a39586614d75d21307a92c1d366a2
mamgjunior <mamgjunior@hotmail.com>
2021-11-05 23:50:30 -0300

    implementando a funcionalidade de reset de senhas

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from . import views
 
@@ -8,15 +8,22 @@ urlpatterns = [
     # path('login/', views.user_login, name='login'),
     path('', views.dashboard, name='dashboard'),
 
-    # url para fazer o login na aplicação
-    path('login/', auth_views.LoginView.as_view(), name='login'),
+    # Este padrão de urls substitui todos os colocados abaixo
+    path('', include('django.contrib.auth.urls')),
+]
 
-    # url para fazer o logout da aplicação
+"""
+    # urls para fazer o login e logout na aplicação
+    path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
-    # url para alteração de senha
+    # urls para alteração de senha e confirmação da alteração
     path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
-
-    # url que confirma a alteração de senha
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
-]
+
+    # urls para reiniciar senhas
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+"""
