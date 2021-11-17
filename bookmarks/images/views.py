203e5b7db4f6d875ecfadc4f2669bca751553a95
mamgjunior <mamgjunior@hotmail.com>
2021-11-17 15:12:16 -0300

    implementando marcação de imagens

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django import forms
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import ImageCreateForm
 
 # Create your views here.
+
+@login_required
+def image_create(request):
+    if request.method == 'POST':
+        form = ImageCreateForm(data=request.POST)
+        if form.is_valid():
+            # salvando os dados do formulário
+            cd = form.cleaned_data
+            new_item = form.save(commit=False)
+
+            # atribui o usuário atual ao item
+            new_item.user = request.user
+            new_item.save()
+            messages.success(request, 'Image added successfully')
+
+            # redireciona para view de detalhes do novo item
+            return redirect(new_item.get_absolute_url())
+    else:
+        # cria o formulário com os dados fornecidos pelo bookmarklet via GET
+        form = ImageCreateForm(data=request.GET)
+    
+    return render(request, 'images/image/create.html', {'section': 'images', 'form': form})
+
