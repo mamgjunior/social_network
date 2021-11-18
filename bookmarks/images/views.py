d1da7922be2e263950e6f6ecc2ebee7ab8a4ac20
mamgjunior <mamgjunior@hotmail.com>
2021-11-18 11:25:52 -0300

    implamentando um bookmarklet com jquery

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import ImageCreateForm
+from django.shortcuts import get_object_or_404
+from .models import Image
 
 # Create your views here.
 
@@ -28,3 +30,7 @@ def image_create(request):
     
     return render(request, 'images/image/create.html', {'section': 'images', 'form': form})
 
+def image_detail(request, id, slug):
+    image = get_object_or_404(Image, id=id, slug=slug)
+    template_name = 'images/image/detail.html'
+    return render(request, template_name, {'section': 'images', 'image': image})
